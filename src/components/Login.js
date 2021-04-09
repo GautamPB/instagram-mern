48bb5210bb69e317207f6086bfdbf7a98dcd9a9f
Gautam <pbg2402@gmail.com>
2021-04-10 01:24:41 +0530

    Added: Login page styles, Right bar in homepage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import '../styles/Login.css'
+import { Button } from '@material-ui/core'
+import FacebookIcon from '@material-ui/icons/Facebook'
 import login__picture from '../assets/login_picture.PNG'
 
 const Login = () => {
     return (
         <div className="login">
-            <div className="login__body">
-                <div className="login__picture">
-                    <img src={login__picture} alt="" />
+            <div className="login__picture">
+                <img
+                    src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                    alt=""
+                />
+            </div>
+
+            <div className="login__phone">
+                <img src={login__picture} alt="" />
+            </div>
+
+            <div className="login__options">
+                <div className="login__facebook login__option">
+                    <Button className="login__facebook">
+                        <FacebookIcon />
+                        Login with Facebook
+                    </Button>
                 </div>
 
-                <div className="login__details">
+                <div className="login__google login__option">
                     <img
-                        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                        src="http://pngimg.com/uploads/google/google_PNG19635.png"
                         alt=""
                     />
+                    <Button className="login__google">Login with Google</Button>
                 </div>
             </div>
         </div>
---
+++ src/components/Recommended.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Recommended.css'
+import { Avatar } from '@material-ui/core'
+
+const Recommended = ({ profile, username, text, button_text }) => {
+    return (
+        <div className="recommended">
+            <Avatar src={profile} />
+            <div className="recommended__details">
+                <h6>{username}</h6>
+                <p>{text}</p>
+            </div>
+
+            <div className="recommended__button">
+                <button>{button_text}</button>
+            </div>
+        </div>
+    )
+}
+
+export default Recommended
